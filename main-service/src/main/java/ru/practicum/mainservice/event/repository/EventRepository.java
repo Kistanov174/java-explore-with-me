3d1d59f96230c58fabee6453855fd5479cfa70c9
A.Kistanov <aleksey.kistanov@yandex.ru>
2023-11-30 18:36:18 +0500

    feat: 14

--- main-service/src/main/java/ru/practicum/mainservice/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/mainservice/event/repository/EventRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.mainservice.event.model.Event;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
