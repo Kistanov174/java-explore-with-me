43778779df6a6b34fb2fd9e09d6f8a5b4065f008
A.Kistanov <aleksey.kistanov@yandex.ru>
2023-11-30 18:48:07 +0500

    feat: 15

--- main-service/src/main/java/ru/practicum/mainservice/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/mainservice/event/repository/EventRepository.java
@@ -1,14 +1,14 @@
 package ru.practicum.mainservice.event.repository;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.mainservice.event.model.Event;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long>, QuerydslPredicateExecutor<Event> {
-    List<Event> findEventsByInitiatorId(Long userId, Pageable page);
+    List<Event> findEventsByInitiatorId(Long userId, PageRequest page);
 
     List<Event> findAllByCategory_Id(Long id);
 }
