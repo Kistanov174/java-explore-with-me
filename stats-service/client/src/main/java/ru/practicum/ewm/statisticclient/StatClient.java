ae71dbd3cab2f00e56bb66bfa2b24fa0b4f2b3ec
A.Kistanov <aleksey.kistanov@yandex.ru>
2023-12-05 18:43:25 +0500

    feat: 12

--- stats-service/client/src/main/java/ru/practicum/ewm/statisticclient/StatClient.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/statisticclient/StatClient.java
@@ -1,17 +1,17 @@
 package ru.practicum.ewm.statisticclient;
 
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-//import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.ewm.statdto.EndpointHitDto;
 import ru.practicum.ewm.statdto.ViewStatsDto;
 import javax.servlet.http.HttpServletRequest;
@@ -19,18 +19,17 @@ import java.util.List;
 
 @Service
 public class StatClient {
-    @Value("${SERVER_URL}")String url;
-//    private final RestTemplate template;
-//    public StatClient(@Value("${SERVER_URL}")String url,
-//                      RestTemplateBuilder templateBuilder) {
-//        this.template = templateBuilder
-//                .uriTemplateHandler(new DefaultUriBuilderFactory(url))
-//                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-//                .build();
-//    }
+    private final RestTemplate template;
+
+    public StatClient(@Value("${SERVER_URL}")String url,
+                      RestTemplateBuilder templateBuilder) {
+        this.template = templateBuilder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(url))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+    }
 
     public void addHit(HttpServletRequest request) {
-        RestTemplate template = new RestTemplate();
         EndpointHitDto endpointHitDto = new EndpointHitDto(
                 null,
                 "main-service",
@@ -38,7 +37,7 @@ public class StatClient {
                 request.getRemoteAddr(),
                 null
         );
-        template.postForEntity(url + "/hit",
+        template.postForEntity("/hit",
                 new HttpEntity<>(endpointHitDto),
                 EndpointHitDto.class);
     }
@@ -47,8 +46,7 @@ public class StatClient {
                                                       String end,
                                                       String[] uris,
                                                       boolean unique) {
-        RestTemplate template = new RestTemplate();
-        return template.exchange(url + "/stats?start={start}&end={end}&uris={uris}&unique={unique}",
+        return template.exchange("/stats?start={start}&end={end}&uris={uris}&unique={unique}",
                 HttpMethod.GET,
                 getHttpEntity(),
                 new ParameterizedTypeReference<>() {
